71d7ae8826eb428833d6407bdec208ca65ae1b65
jiho <ziofront@gmail.com>
2019-02-10 01:08:38 +0900

    - spring security - thymeleaf - dataloader - rename class User to Member

--- src/main/java/com/ziofront/challenge/security/UserDetailsImpl.java
+++ src/main/java/com/ziofront/challenge/security/UserDetailsImpl.java
@@ -9,9 +9,6 @@ import java.util.Collection;
 
 public class UserDetailsImpl extends User {
 
-    /* security 에서 제공하는 Member 객체를 상속받아야함 난 이부분에서 진짜 많이 헤맴 ... */
-
-
     public UserDetailsImpl(String username, String password, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
     }
@@ -22,7 +19,6 @@ public class UserDetailsImpl extends User {
         super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
     }
 
-    /* 사실 여기선 이거밖에 안씀. 위에 두개는 어떤거 하는지 모르겠는데 좀더 연구한 후에... */
     public UserDetailsImpl(String email, String password, String s) {
         super(email, password, AuthorityUtils.createAuthorityList(s));
     }
