743ccca8ae514ac697d77ef4bab9b6523d434145
Jiho <ziofront@gmail.com>
2019-02-18 22:07:35 +0900

    사용하지 않는 소스 제거

--- src/main/java/com/ziofront/challenge/security/WebSecurityConfig.java
+++ src/main/java/com/ziofront/challenge/security/WebSecurityConfig.java
@@ -1,22 +1,17 @@
 package com.ziofront.challenge.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 /**
  * @author jiho
@@ -57,11 +52,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/oauth/**");
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         // super.configure(http);
         http.httpBasic().and()
@@ -90,23 +80,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
 
                 .authorizeRequests()
-                .antMatchers("/api/v1/member/login", "/oauth/token").permitAll()
-                .and()
-
-                .authorizeRequests()
-                .antMatchers("/h2-console/*"
-                        , "/api/v1/member/login"
-                        , "/oauth/token")
+                .antMatchers("/h2-console/*")
                 .anonymous().anyRequest().permitAll()
 
                 .and()
                 .headers().frameOptions().sameOrigin()
 
                 .and()
-                .csrf().ignoringAntMatchers("/h2-console/**"
-                , "/api/v1/member/login"
-                , "/oauth/token"
-        )
+                .csrf().ignoringAntMatchers("/h2-console/**")
                 .disable()
         ;
 
@@ -114,18 +95,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
 }
